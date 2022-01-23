6858bcdccb9200efd4b7b19bfb209a9400b98f2a
ben99933 <52560120+ben99933@users.noreply.github.com>
2022-01-23 21:57:13 +0800

    Create a004.cpp

---
+++ 20220126/a004.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int year;
+	while (cin >> year) {
+		if (year % 400 == 0) {
+			cout << "閏年" << endl;
+			
+		}
+		else if (year % 4 == 0 && year % 100 != 0) {
+			cout << "閏年" <<endl;
+			
+		}
+		else {
+			cout << "平年"<< endl;
+			
+		}
+	}
+	return 0;
+}
+
+
+	
+
+
