b7807b5e46d160b97662d5a34d7b75259ed6a0e1
ben99933 <52560120+ben99933@users.noreply.github.com>
2022-01-23 21:52:30 +0800

    Create a003.cpp

---
+++ 20220126/a003.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	long month;
+	long day;
+	cin >> month;
+	cin >> day;
+
+	long s = (month * 2 + day) % 3;
+	string result = s == 0 ? "普通" : s == 1 ? "吉" : "大吉";
+	cout << result;
+	return 0;
+}
+
+
+	
+
