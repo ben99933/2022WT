0206ef7b0b3aa70389ae5e27d4ef6ce7e74c3080
ben99933 <52560120+ben99933@users.noreply.github.com>
2022-01-24 20:07:22 +0800

    Create a024.cpp

---
+++ 20220126/a024.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include<map>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+long gcd(long a, long b) {
+	if (b > a)return gcd(b, a);
+	if (a %b == 0)return b;
+	return gcd(b, a - b);
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	long a, b;
+	cin >> a >> b;
+	cout << gcd(a, b);
+	return 0;
+}
+
+
+	
+
+
