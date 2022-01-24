9058a7f18df404979d88e9b9ab01d58297b3178c
ben99933 <52560120+ben99933@users.noreply.github.com>
2022-01-24 20:11:17 +0800

    Create a034.cpp

---
+++ 20220126/a034.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include<map>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	long a;
+	while (cin >> a) {
+		string s = "";
+		while (a >= 1) {
+			if (a % 2 == 0) {
+				
+				s = "0" + s;
+			}
+			else {
+				s = "1" + s;
+			}
+			a /= 2;
+		}
+		cout << s << endl;
+
+	}
+
+	return 0;
+}
+
+
+	
+
