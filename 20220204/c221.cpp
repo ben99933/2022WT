c469dfc2566cc81de03f3025eae8a667edbb7614
ben99933 <52560120+ben99933@users.noreply.github.com>
2022-02-02 12:29:36 +0800

    Create c221.cpp

---
+++ 20220204/c221.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    
+    cout <<INT32_MAX << " " << INT32_MAX;
+    
+    return 0;
+}
